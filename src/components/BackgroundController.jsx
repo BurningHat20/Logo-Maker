9122599f60cae201a3d71d2c451cf7d8957d7613
BurningHat12 <yashgohel395@gmail.com>
2024-05-10 13:37:43 +0530

    feat: Add @radix-ui/react-slider dependency and update component styles
    - Add @radix-ui/react-slider dependency to package.json
    - Update BackgroundController component to use max-h-screen class
    - Update Header component to use items-center class

--- src/components/BackgroundController.jsx
+++ src/components/BackgroundController.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BackgroundController = () => {
   return (
-    <div>BackgroundController</div>
+    <div className="max-h-screen">BackgroundController</div>
     // ddd
   );
 };
